5a172e9c37fef4b18f6adaa45733bf6be53c7c7d
Artem Parashchenko <artem99paraschenko@gmail.com>
2022-04-27 17:19:37 +0300

    add Puzzle Game

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ helloBtn = document.querySelector('#hello-btn');
 
 
 helloBtn.addEventListener('click', () => {
-  greating.classList.add('hide')
+  greating.classList.add('hide');
+  document.body.style = null;
   setTimeout(() => greating.style.display = 'none',
   1000);
   document.body.style.overflow = 'overlay';
